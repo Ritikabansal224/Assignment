3eec54780d575e666fb02819c409acf5cc546f33
Ritikabansal224 <49287044+Ritikabansal224@users.noreply.github.com>
2020-09-08 22:10:27 +0530

    Update remove duplicate element by own function.py

--- remove duplicate element by own function.py
+++ remove duplicate element by own function.py
@@ -1,14 +1,14 @@
 def UsingOwnFunction(arr):
     mp = dict()
-    ans = list()
+    a = list()
     for i in arr:
         if(i not in mp):
             ans.append(i)
             mp[i] = 1
-    return ans
+    return a
     
 arr = list(map(int, input().split()))
 
-ans = UsingOwnFunction(arr)
+a = UsingOwnFunction(arr)
 
-print(ans)
+print(a)
